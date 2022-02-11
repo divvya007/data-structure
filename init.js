819b9ac2cd4d1ae381d1a2e10af06384cce95330
divvya007 <divvyatripaathi@gmail.com>
2022-02-11 11:58:07 +0000

    stack data structure implementation

--- init.js
+++ init.js
@@ -1,1 +1,27 @@
 // stack implementation
+
+class Stack {
+  #stackedArray = [];
+  constructor(stackedArray) {
+    this.#stackedArray = [];
+  }
+  push(value) {
+    let firstValue;
+    firstValue = this.#stackedArray.push(value);
+    return firstValue;
+  }
+  pop() {
+    if (this.#stackedArray.length === 0) {
+      alert("stack is empty add more values");
+    } else {
+      let lastValue;
+      lastValue = this.#stackedArray.pop();
+      return lastValue;
+    }
+  }
+}
+
+let someElementToBeStacked = new Stack();
+
+console.log(someElementToBeStacked);
+console.log(Stack.stackedArray);
