b9cfd6f3505411a7f938c45849e6973a300c098b
Digvijay Upadhyay <digvijay.u20@gmail.com>
2022-02-13 22:11:58 +0000

    refactor stack

--- init.js
+++ init.js
@@ -2,28 +2,51 @@
 
 class Stack {
   #stackedArrayStore = [];
-  constructor(stackedArrayStore) {
-    this.#stackedArrayStore = [];
-  }
+
   push(value) {
     if (value === undefined) {
-      alert("cannot add undefined");
-    } else {
-      this.#stackedArrayStore.push(value);
-      return this.#stackedArrayStore[this.#stackedArrayStore.length - 1];
+      throw new Error("Cannot add undefined");
     }
+
+    if (value === null) {
+      throw new Error("Cannot add null");
+    }
+
+    this.#stackedArrayStore.push(value);
+    console.log("push", value);
+    console.log("updated stack", this.#stackedArrayStore);
+    return this.#stackedArrayStore[this.#stackedArrayStore.length - 1];
   }
+
   pop() {
     if (this.#stackedArrayStore.length === 0) {
-      alert(null);
-    } else {
-      let lastValue;
-      lastValue = this.#stackedArrayStore.pop();
-      return lastValue;
+      return null;
     }
+
+    let value = this.#stackedArrayStore.pop();
+    console.log("pop", value);
+    console.log("updated stack", this.#stackedArrayStore);
+    return value;
   }
 }
 
-let someElementToBeStacked = new Stack();
+let myStack = new Stack();
 
-console.log(someElementToBeStacked);
+myStack.push(5);
+myStack.push(8);
+myStack.push(2);
+
+myStack.pop();
+myStack.pop();
+
+myStack.push("str");
+myStack.push({ a: "b"});
+
+myStack.pop();
+myStack.pop();
+myStack.pop();
+myStack.pop();
+
+// error
+myStack.push();
+myStack.push(null);
