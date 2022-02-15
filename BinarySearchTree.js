70eaeefea20461c3414fceab4298ccd9c23a5316
divvya007 <divvyatripaathi@gmail.com>
2022-02-15 16:14:14 +0000

    nothing to show

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -3,55 +3,25 @@
 console.log("..........binary tree search.............");
 console.log("..........binary tree search.............");
 class Node {
-  constructor(data) {
+  constructor(data, left, right, show) {
     this.data = data;
-    this.left = null;
-    this.right = null;
+    this.left = left;
+    this.right = right;
+    this.show = show;
   }
 }
-function insert(root, key) {
-  console.log(root, key);
-  if (root === null) {
-    console.log("before:", this);
-    root = new Node(key);
-    console.log("root inside if:", root);
-    console.log("after:", this);
-    return;
-  }
-  let q = [];
-  console.log("q.push(root):", q.push(root));
-  q.push(root);
+function insert(data) {
+  let n = new Node(data, null, null);
 
-  while (q.length !== 0) {
-    console.log("q.length", q.length);
-    root = q.pop();
-    console.log("root.pop:", root);
-    console.log("q:", q);
-
-    if (root.left === null) {
-      root.left = new Node(key);
-      break;
-    } else {
-      q.push(root.left);
-      console.log(q.push(root.left));
-    }
-    if (root.right === null) {
-      root.right = new Node(key);
-      break;
-    } else {
-      q.push(root.right);
-      console.log(q.push(root.right));
-    }
+  debugger;
+  if (this.root === null) {
+    console.log("this.root1:", this.root);
+    this.root = n;
+    console.log("this.root2:", this.root);
+  } else {
+    let current = this.root;
+    console.log(current);
   }
 }
 
-const node = new Node(null);
-// node.left = new Node(4);
-// node.right = new Node(6);
-// node.left.left = new Node(3);
-// node.right.left = new Node(13);
-// node.right.right = new Node(14);
-let key = 12;
-// debugger;
-console.log(node);
-console.log(insert(node, key));
+console.log(insert(1));
